9378f9019288544932b37cb7073d6862eff9c1af
Merry <svgr1808@outlook.de>
2023-12-06 21:40:23 +0100

    Add autogenerate plugin.yml plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
 plugins {
     id("java")
     id("io.freefair.lombok") version "8.4"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0" // Generates the plugin.yml when building the project
 }
 
 group = "net.quantrax"
@@ -17,4 +20,23 @@ dependencies {
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+bukkit {
+    main = "net.quantrax.citybuild.CityBuildPlugin"
+    foliaSupported = false
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+    author = "MerryChrismas"
+    contributors = listOf("ExceptionThread", "DeRio_", "ByTRYO")
+
+    commands {
+        /*
+        Register your commands here.
+
+        register("test") {
+            description = "This is a test command!"
+            aliases = listOf("t")
+        }
+         */
+    }
 }
