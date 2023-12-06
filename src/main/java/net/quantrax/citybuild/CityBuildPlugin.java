09ea39014055c3468c42dfaaa0a57a80f7faae20
Dario P <derioreal63@gmail.com>
2023-12-06 21:55:55 +0100

    update main class

--- src/main/java/net/quantrax/citybuild/CityBuildPlugin.java
+++ src/main/java/net/quantrax/citybuild/CityBuildPlugin.java
@@ -1,7 +1,16 @@
 package net.quantrax.citybuild;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CityBuildPlugin extends JavaPlugin {
+
+    @Getter
+    private static CityBuildPlugin instance;
+
+    @Override
+    public void onLoad() {
+        CityBuildPlugin.instance = this;
+    }
 
 }
