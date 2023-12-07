e054fefc08873e29a337cf9f10a3c67d5c8c1264
Riccardo <76207588+ExceptionThread@users.noreply.github.com>
2023-12-07 18:09:45 +0100

    Add Enable/Disable Methode (#1)

--- src/main/java/net/quantrax/citybuild/CityBuildPlugin.java
+++ src/main/java/net/quantrax/citybuild/CityBuildPlugin.java
@@ -1,7 +1,17 @@
 package net.quantrax.citybuild;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CityBuildPlugin extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
