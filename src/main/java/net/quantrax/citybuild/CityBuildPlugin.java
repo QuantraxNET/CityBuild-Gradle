0cafd33b1c64396962b25e1406a07ad09f12e811
Riccardo <76207588+ExceptionThread@users.noreply.github.com>
2023-12-07 18:36:34 +0100

    change deprecated newInstance (#3)

--- src/main/java/net/quantrax/citybuild/CityBuildPlugin.java
+++ src/main/java/net/quantrax/citybuild/CityBuildPlugin.java
@@ -1,11 +1,8 @@
 package net.quantrax.citybuild;
 
-import com.google.common.collect.UnmodifiableIterator;
 import com.google.common.reflect.ClassPath;
 import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CityBuildPlugin extends JavaPlugin {
@@ -15,22 +12,20 @@ public class CityBuildPlugin extends JavaPlugin {
 
         registerListener();
     }
+
     @Override
     public void onDisable() {
 
     }
+
     public void registerListener() {
         try {
-            for (UnmodifiableIterator<ClassPath.ClassInfo> unmodifiableIterator =
-                 ClassPath.from(getClassLoader())
-                         .getTopLevelClasses("net.quantrax.citybuild.listener")
-                         .iterator();
-                 unmodifiableIterator.hasNext(); ) {
-                    ClassPath.ClassInfo classInfo = unmodifiableIterator.next();
-                    Class<?> clazz = Class.forName(classInfo.getName());
-                    if (Listener.class.isAssignableFrom(clazz))
-                        Bukkit.getPluginManager().registerEvents((Listener) clazz.newInstance(), (Plugin) this);
-                }
+            for (ClassPath.ClassInfo classInfo : ClassPath.from(getClassLoader())
+                    .getTopLevelClasses("net.quantrax.citybuild.listener")) {
+                Class<?> clazz = Class.forName(classInfo.getName());
+                if (Listener.class.isAssignableFrom(clazz))
+                    Bukkit.getPluginManager().registerEvents((Listener) clazz.getDeclaredConstructor().newInstance(), this);
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
